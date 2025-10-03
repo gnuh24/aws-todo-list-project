ebc554351758d00e9cdeb02286fc21600194c80f
gnuh24 <hungnt.020404@gmail.com>
2025-10-03 18:07:37 +0700

    fix(script setup docker): Fix lỗi sleep trên Window

--- todolist-database/redis-init.sh
+++ todolist-database/redis-init.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
-
-# Start Redis server in background
 redis-server &
-sleep 2  # đợi Redis khởi động
 
-# Tạo các key email_exist:<email>
+until redis-cli ping 2>/dev/null | grep -q PONG; do
+  echo "Waiting for Redis..."
+  sleep 1
+done
+
 redis-cli SET email_exist:admin@gmail.com 1
 redis-cli SET email_exist:user1@gmail.com 1
 redis-cli SET email_exist:user2@gmail.com 1
 
-# Giữ Redis process chạy
 wait
