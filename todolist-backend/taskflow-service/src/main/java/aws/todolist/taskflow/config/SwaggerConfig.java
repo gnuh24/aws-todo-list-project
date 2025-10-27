d506f0a8e8de49e556ef4696e34265073e065be9
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2025-10-27 20:52:21 +0700

    chore: Cập nhật swagger cho taskflow

--- todolist-backend/taskflow-service/src/main/java/aws/todolist/taskflow/config/SwaggerConfig.java
+++ todolist-backend/taskflow-service/src/main/java/aws/todolist/taskflow/config/SwaggerConfig.java
@@ -1,20 +1,24 @@
 package aws.todolist.taskflow.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-	
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-			    .info(new Info()
-					.title("Ecommerce microservice API Documentation")
-					.version("1.0")
-					.description("API Documentation for EC Project")
-			    );
-		}
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .name("Bearer Authentication")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
