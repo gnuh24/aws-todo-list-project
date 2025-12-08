881d3935a8f613d08511d8adf6d691a5f5416647
gnuh24 <hungnt.020404@gmail.com>
2025-12-08 20:56:06 +0700

    Fix: Lỗi syntax database

--- todolist-database/mysql-init.sql
+++ todolist-database/mysql-init.sql
@@ -166,7 +166,7 @@ VALUES
 ('22222222-2222-2222-2222-222222222222', 'user1@gmail.com', '$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', NULL, 'User One', 'USER', 'ACTIVE', NOW(), NOW(), NULL, 0, 1),
 ('33333333-3333-3333-3333-333333333333', 'user2@gmail.com', '$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', NULL, 'User Two', 'USER', 'ACTIVE', NOW(), NOW(), NULL, 0, 0),
 ('0c78b06c-9597-4b88-b5aa-f3c13dbf1a44', 'user3@gmail.com', '$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', NULL, 'User Four', 'USER', 'INACTIVE', NOW(), NOW(), NULL, 0, 1),
-('c4f6d12b-5e84-4d1a-8ca4-421e83a4e555', 'user4@gmail.com', '$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', NULL, 'User Five', 'USER', 'ACTIVE', NOW(), NOW(), NULL, 0, 1),
+('c4f6d12b-5e84-4d1a-8ca4-421e83a4e555', 'user4@gmail.com', '$2a$10$W2neF9.6Agi6kAKVq8q3fec5dHW8KUA.b0VSIGdIZyUravfLpyIFi', NULL, 'User Five', 'USER', 'ACTIVE', NOW(), NOW(), NULL, 0, 1);
 
 INSERT INTO `personal_label` (`id`, `account_id`, `name`, `description`, `created_at`, `updated_at`, `deleted_at`, `is_deleted`)
 VALUES
