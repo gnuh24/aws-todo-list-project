d7a1ee46f246e45d5084f3b01175ea1cf31b48be
Ngô Tuấn Hưng <119871143+gnuh24@users.noreply.github.com>
2025-12-09 18:01:21 +0700

    Fix lỗi thiếu .com trong notification service

--- todolist-backend/notification-service/src/main/java/aws/todolist/notification/service/EmailServiceImpl.java
+++ todolist-backend/notification-service/src/main/java/aws/todolist/notification/service/EmailServiceImpl.java
@@ -21,7 +21,7 @@ public class EmailServiceImpl implements EmailService {
 	
 	@Override
 	public void sendRegistrationUserConfirm(String email, String otp) {
-		String confirmationUrl = "https://sgutodolist/auth/verify-account?otp=" + otp;
+		String confirmationUrl = "https://sgutodolist.com/auth/verify-account?otp=" + otp;
 		
 		String subject = "Xác Nhận Đăng Ký Tài khoản";
 		String content = getEmailContentForRegistration(confirmationUrl);
@@ -191,4 +191,5 @@ public class EmailServiceImpl implements EmailService {
 	}
 	
 }
+
 
