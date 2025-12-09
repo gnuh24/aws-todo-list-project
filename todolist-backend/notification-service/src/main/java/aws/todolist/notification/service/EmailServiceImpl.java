b1834b8393b779733809b6d62cc831d5adb8fbb1
Ngô Tuấn Hưng <119871143+gnuh24@users.noreply.github.com>
2025-12-09 17:24:58 +0700

    Refactor EmailServiceImpl for improved email content (local -> prod)
    Updated email content URLs and improved HTML structure for better styling.

--- todolist-backend/notification-service/src/main/java/aws/todolist/notification/service/EmailServiceImpl.java
+++ todolist-backend/notification-service/src/main/java/aws/todolist/notification/service/EmailServiceImpl.java
@@ -21,7 +21,7 @@ public class EmailServiceImpl implements EmailService {
 	
 	@Override
 	public void sendRegistrationUserConfirm(String email, String otp) {
-		String confirmationUrl = "http://localhost:3000/auth/verify-account?otp=" + otp;
+		String confirmationUrl = "https://sgutodolist/auth/verify-account?otp=" + otp;
 		
 		String subject = "Xác Nhận Đăng Ký Tài khoản";
 		String content = getEmailContentForRegistration(confirmationUrl);
@@ -191,3 +191,4 @@ public class EmailServiceImpl implements EmailService {
 	}
 	
 }
+
