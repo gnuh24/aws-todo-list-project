9aafe24c205121aad236ffc4448f58866b802f27
adminmoi <pcanhtuandat@gmail.com>
2026-01-02 19:59:06 +0700

    init: add chat service (clean, no frontend artifacts)

---
+++ todolist-backend/chat-service/src/test/java/aws/todolist/chat/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.todolist.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
